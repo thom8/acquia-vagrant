82738cc84ae7dc29ba0308dc054d8a52d0ead5bc
Thom Toogood <thom.toogood@eurekareport.com.au>
2015-08-17 13:38:41 +1000

    Remove sql-drop as drush site-install will drop existing db.

--- vagrant-includes/site_install.sh
+++ vagrant-includes/site_install.sh
@@ -10,13 +10,12 @@ if [ ! -f /home/vagrant/siteinstalled ] ; then
   echo "Starting Drupal installation. This takes a few mins."
 
   # Tidy up
-  drush --root=/drupal sql-drop -y >/dev/null
   sudo chmod 775 /drupal/sites/default/ >/dev/null
   sudo rm -rf /drupal/sites/default/services.yml >/dev/null
   sudo rm -rf /drupal/sites/default/settings.php >/dev/null
 
   # Install Drupal
-  drush --root=/drupal si -y -q \
+  drush --root=/drupal site-install -y -q \
   --db-url="mysql://root:root@localhost:3306/$dbName" \
   --account-name="$drupalUser" \
   --account-pass="$drupalPass" \
